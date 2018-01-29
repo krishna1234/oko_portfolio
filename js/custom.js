cedbe179a3213be834c21cf3f7e167b9e3f93581
krishna.s <krishna.s@crediwatch.com>
2018-01-30 05:03:29 +0530

    landing page

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {;
+    $('#acc-slider').liteAccordion({
+        containerWidth: 960,
+        containerHeight: 600,
+        headerWidth: 48,
+        responsive: true,
+        autoScaleImages: false,
+        activateOn: 'mouseover',
+        pauseOnHover: true,
+        theme: 'stitch',
+        firstSlide: 1,
+        linkable: false
+    });
+});
